c176c9ad76d65730969db0ddcceed49591c11432
Raffaello Damgaard <raffaello.neves@sharecare.com>
2018-10-24 20:10:05 -0300

    Populando o NoteList.

--- missaokotlinproject/app/src/main/java/br/com/sharebook/missao_kotlin_project/NoteListActivity.kt
+++ missaokotlinproject/app/src/main/java/br/com/sharebook/missao_kotlin_project/NoteListActivity.kt
@@ -4,8 +4,10 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter
 
 import kotlinx.android.synthetic.main.activity_note_list.*
+import kotlinx.android.synthetic.main.content_note_list.*
 
 class NoteListActivity : AppCompatActivity() {
 
@@ -18,6 +20,8 @@ class NoteListActivity : AppCompatActivity() {
             val activityIntent = Intent(this, MainActivity::class.java)
             startActivity(activityIntent)
         }
+
+        listNotes.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, DataManager.notes)
     }
 
 }
