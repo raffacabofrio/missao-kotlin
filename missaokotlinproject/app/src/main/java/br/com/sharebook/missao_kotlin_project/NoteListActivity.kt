1b2d02bb37f3a39950d4256f238e873795fb9677
Raffaello Damgaard <raffaello.neves@sharecare.com>
2018-10-24 19:47:50 -0300

    Uma activity abre outra activity

--- missaokotlinproject/app/src/main/java/br/com/sharebook/missao_kotlin_project/NoteListActivity.kt
+++ missaokotlinproject/app/src/main/java/br/com/sharebook/missao_kotlin_project/NoteListActivity.kt
@@ -1,5 +1,6 @@
 package br.com.sharebook.missao_kotlin_project
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
@@ -14,8 +15,8 @@ class NoteListActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+            val activityIntent = Intent(this, MainActivity::class.java)
+            startActivity(activityIntent)
         }
     }
 
